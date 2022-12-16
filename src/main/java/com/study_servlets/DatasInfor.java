8c4e926b5dcd811dbb6481fde43179b00b47b321
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2022-12-16 11:58:41 +0900

    hyun

--- src/main/java/com/study_servlets/DatasInfor.java
+++ src/main/java/com/study_servlets/DatasInfor.java
@@ -1,21 +1,18 @@
-package com.kh.jung.study_servlets;
-
+package com.study_servlets;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.kh.jung.study_servlets.learn_servlets.beans.MemberBean;
+
 public class DatasInfor {
-
-    // search 인풋
     public HashMap<String, String> getSearchFormData() {
-
         HashMap<String, String> searchForm = new HashMap<String, String>();
         searchForm.put("search_key", "Search Title");
-        searchForm.put("name", "진보경!");
+        searchForm.put("name", "요주의랩!");
         searchForm.put("id", "ID0001");
         return searchForm;
     }
 
-    // 테이블
     public ArrayList<String> getTablesListWithString() {
         ArrayList<String> tablesListWithString = new ArrayList<String>();
         tablesListWithString.add("@mdo");
@@ -24,17 +21,24 @@ public class DatasInfor {
         return tablesListWithString;
     }
 
-    // 다른유형 합치기
     public HashMap<String, Object> getBundlesData() {
         DatasInfor datasInfor = new DatasInfor();
         HashMap<String, String> searchForm = datasInfor.getSearchFormData();
         ArrayList<String> tablesListWithString = datasInfor.getTablesListWithString();
 
-        // 앞에만 제대로 선언하면 가능. 둘 데이터를 합침
         HashMap<String, Object> bundlesData = new HashMap<>();
-        // 키는 다른이름 가능
         bundlesData.put("searchForm", searchForm);
         bundlesData.put("tablesListWithString", tablesListWithString);
         return bundlesData;
     }
+
+    public MemberBean getDataWithMamberBean() {
+        MemberBean memberBean = new MemberBean();
+        memberBean.setFirstName("Mark");
+        memberBean.setSecondName("Otto");
+        memberBean.setHandleName("@mdo");
+
+        return memberBean;
+    }
+
 }

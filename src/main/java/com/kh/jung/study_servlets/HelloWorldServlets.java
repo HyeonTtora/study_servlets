59b45a01ffbe02a838e5b399fe0c1e64497a2fea
현또라 <108624538+HyeonTtora@users.noreply.github.com>
2022-12-16 09:34:16 +0900

    studyServlets

--- src/main/java/com/kh/jung/study_servlets/HelloWorldServlets.java
+++ src/main/java/com/kh/jung/study_servlets/HelloWorldServlets.java
@@ -10,20 +10,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/helloWorldServlets")
 public class HelloWorldServlets extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-       
-        String message = "HelloWorldServlets ! with MESSAGE !!";
         PrintWriter printWriter = response.getWriter();
-
+        String messege = "Hello World Servlets with Message! ";
         printWriter.println("<html lang='en'>");
         printWriter.println("<head>");
-        printWriter.println("<title>"+message+"</title>");
+        printWriter.println("<title>" + messege + "</title>");
         printWriter.println("</head>");
         printWriter.println("<body>");
-        printWriter.println("<div>"+message+"</div>");
-        printWriter.println("</body>");
-        printWriter.println("</html>");
+        printWriter.println("<div>" + messege + "</div>");
+        printWriter.println("</body></html>");
 
         printWriter.close();
     }
+
 }
